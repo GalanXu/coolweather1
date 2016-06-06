49826c1b37c9ef7b2f2093e45947353456ebbfaa
ccxgn1 <ccxgn1@163.com>
2016-06-06 09:37:30 +0800

    first commit

---
+++ src/com/example/coolw1/model/City.java
@@ -0,0 +1,8 @@
+package com.example.coolw1.model;
+
+public class City {
+	public int id; 
+	public String province; 
+	public String city; 
+	public String district; 
+}
---
+++ src/com/example/coolw1/model/Province.java
@@ -0,0 +1,13 @@
+package com.example.coolw1.model;
+
+import java.util.List;
+
+
+public class Province {
+	public String resultcode;
+	public String reason;
+	public String error_code;
+	public List<City> result;
+	
+
+}
